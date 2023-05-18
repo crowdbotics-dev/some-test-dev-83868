c81cc1c77248d1085946b2b04a61086b870bc116
Robert So <robkso@gmail.com>
2023-05-17 21:44:48 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "some-test-dev-83868",
   "url": "https://some-test-dev-83868.botics.co",
   "api": "https://some-test-dev-83868.botics.co/api/v1",
-  "headerShown": false
+  "headerShown": true
 };
 export const modulesOptions = {
   "@modules/app-menu": {
