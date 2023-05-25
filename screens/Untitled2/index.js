fd54a5dd8682123b6de9aad382a0d281fa34160e
Robert So <robkso@gmail.com>
2023-05-25 09:59:50 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,3 +1,4 @@
+import { Text } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +9,27 @@ const Untitled2 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Text style={styles.TupTqkhX}>{"These are same studios. This one is still using old modules. There is a ticket to refactor this somewhere"}</Text><Text style={styles.cDFQqrBM}>{"The \"onSearch\" function uses the same endpoint and passes in the same search param"}</Text></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  TupTqkhX: {
+    width: 324,
+    height: 225,
+    lineHeight: 36,
+    fontSize: 36,
+    borderRadius: 0
+  },
+  cDFQqrBM: {
+    width: 326,
+    height: 238,
+    lineHeight: 35,
+    fontSize: 36,
+    borderRadius: 0
   }
 });
 export default Untitled2;
